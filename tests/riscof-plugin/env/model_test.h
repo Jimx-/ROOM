df9eef20baff8018314a1569bbefd41bb9423746
Jin Xue <csjinxue@outlook.com>
2026-01-11 11:18:01 +0800

    tests: Add RISCOF plugin

---
+++ tests/riscof-plugin/env/model_test.h
@@ -0,0 +1,98 @@
+#ifndef _COMPLIANCE_MODEL_H
+#define _COMPLIANCE_MODEL_H
+
+#include "generated/platform.h"
+
+// clang-format off
+#define RVMODEL_DATA_SECTION \
+        .pushsection .tohost,"aw",@progbits;                            \
+        .align 8; .global tohost; tohost: .dword 0;                     \
+        .align 8; .global fromhost; fromhost: .dword 0;                 \
+        .popsection;                                                    \
+        .align 8; .global begin_regstate; begin_regstate:               \
+        .word 128;                                                      \
+        .align 8; .global end_regstate; end_regstate:                   \
+        .word 4;
+// clang-format on
+
+#ifndef RVMODEL_PMP_GRAIN
+#define RVMODEL_PMP_GRAIN 0
+#endif
+
+#ifndef RVMODEL_NUM_PMPS
+#define RVMODEL_NUM_PMPS 16
+#endif
+
+// clang-format off
+
+// RV_COMPLIANCE_HALT
+#define RVMODEL_HALT                ;\
+    la t0, begin_signature          ;\
+    la t1, end_signature            ;\
+    sub t1, t1, t0                  ;\
+    andi t2, t0, 63                 ;\
+    sub t0, t0, t2                  ;\
+    add t1, t1, t2                  ;\
+    add t1, t1, t0                  ;\
+    srli t0, t0, 4                  ;\
+    srli t1, t1, 4                  ;\
+    li t2, L2CACHE_FLUSH32_ADDRESS  ;\
+1:                                  ;\
+    sw t0, (t2)                     ;\
+2:                                  ;\
+    lw x1, (t2)                     ;\
+    bnez x1, 2b                     ;\
+    addi t0, t0, 4                  ;\
+    bgtu t1, t0, 1b                 ;\
+    li x1, 1                        ;\
+1:                                  ;\
+    sw x1, tohost, t2               ;\
+    wfi                             ;\
+    j 1b
+
+// clang-format on
+
+#define RVMODEL_BOOT
+
+// clang-format off
+
+//RV_COMPLIANCE_DATA_BEGIN
+#define RVMODEL_DATA_BEGIN                                              \
+  RVMODEL_DATA_SECTION                                                        \
+  .align 4;\
+  .global begin_signature; begin_signature:
+
+//RV_COMPLIANCE_DATA_END
+#define RVMODEL_DATA_END                                                      \
+  .align 4;\
+  .global end_signature; end_signature:
+
+// clang-format on
+
+// RVTEST_IO_INIT
+#define RVMODEL_IO_INIT
+// RVTEST_IO_WRITE_STR
+#define RVMODEL_IO_WRITE_STR(_R, _STR)
+// RVTEST_IO_CHECK
+#define RVMODEL_IO_CHECK()
+// RVTEST_IO_ASSERT_GPR_EQ
+#define RVMODEL_IO_ASSERT_GPR_EQ(_S, _R, _I)
+// RVTEST_IO_ASSERT_SFPR_EQ
+#define RVMODEL_IO_ASSERT_SFPR_EQ(_F, _R, _I)
+// RVTEST_IO_ASSERT_DFPR_EQ
+#define RVMODEL_IO_ASSERT_DFPR_EQ(_D, _R, _I)
+
+#define RVMODEL_SET_MSW_INT \
+    li t1, 1;               \
+    li t2, 0x2000000;       \
+    sw t1, 0(t2);
+
+#define RVMODEL_CLEAR_MSW_INT \
+    li t2, 0x2000000;         \
+    sw x0, 0(t2);
+
+#define RVMODEL_CLEAR_MTIMER_INT
+
+#define RVMODEL_CLEAR_MEXT_INT
+
+#endif // _COMPLIANCE_MODEL_H
---
+++ tests/riscof-plugin/riscof_room_rtlsim.py
@@ -0,0 +1,173 @@
+import os
+import re
+import shutil
+import subprocess
+import shlex
+import logging
+import random
+import string
+from string import Template
+import sys
+
+import riscof.utils as utils
+from riscof.pluginTemplate import pluginTemplate
+
+logger = logging.getLogger()
+
+
+class room_rtlsim(pluginTemplate):
+    __model__ = "room"
+
+    __version__ = "XXX"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        config = kwargs.get('config')
+
+        if config is None:
+            print("Please enter input file paths in configuration.")
+            raise SystemExit(1)
+
+        self.room_root = config['room_root']
+        self.sd_image = config['sd_image']
+
+        self.dut_exe = os.path.join(self.room_root, 'build', 'rtlsim',
+                                    'rtlsim')
+
+        self.num_jobs = str(config['jobs'] if 'jobs' in config else 1)
+
+        self.pluginpath = os.path.abspath(config['pluginpath'])
+
+        self.isa_spec = os.path.abspath(config['ispec'])
+        self.platform_spec = os.path.abspath(config['pspec'])
+
+        if 'target_run' in config and config['target_run'] == '0':
+            self.target_run = False
+        else:
+            self.target_run = True
+
+    def initialise(self, suite, work_dir, archtest_env):
+        self.work_dir = work_dir
+        self.suite_dir = suite
+
+        self.compile_cmd = 'riscv{1}-unknown-elf-gcc -march={0} \
+         -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g\
+         -T ' + self.pluginpath + '/env/link.ld\
+         -I ' + self.pluginpath + '/env/\
+         -I ' + self.room_root + '/include/\
+         -I ' + archtest_env + ' {2} -o {3} {4}'
+
+    def build(self, isa_yaml, platform_yaml):
+        ispec = utils.load_yaml(isa_yaml)['hart0']
+
+        self.xlen = ('64' if 64 in ispec['supported_xlen'] else '32')
+
+        self.isa = 'rv' + self.xlen
+        if "I" in ispec["ISA"]:
+            self.isa += 'i'
+        if "E" in ispec["ISA"]:
+            self.isa += 'e'
+        if "M" in ispec["ISA"]:
+            self.isa += 'm'
+        if "A" in ispec["ISA"]:
+            self.isa += 'a'
+        if "F" in ispec["ISA"]:
+            self.isa += 'f'
+        if "D" in ispec["ISA"]:
+            self.isa += 'd'
+        if "Q" in ispec["ISA"]:
+            self.isa += 'q'
+        if "C" in ispec["ISA"]:
+            self.isa += 'c'
+        if "V" in ispec["ISA"]:
+            self.isa += 'v'
+        if "Zicsr" in ispec["ISA"]:
+            self.isa += '_Zicsr'
+        if "Zicond" in ispec["ISA"]:
+            self.isa += '_Zicond'
+        if "Zicboz" in ispec["ISA"]:
+            self.isa += '_Zicboz'
+        if "Zimop" in ispec["ISA"]:
+            self.isa += '_Zimop'
+        if "Zfa" in ispec["ISA"]:
+            self.isa += '_Zfa'
+        if "Zfh" in ispec["ISA"]:
+            self.isa += '_Zfh'
+        if "Zca" in ispec["ISA"]:
+            self.isa += '_Zca'
+        if "Zcb" in ispec["ISA"]:
+            self.isa += '_Zcb'
+        if "Zcmop" in ispec["ISA"]:
+            self.isa += '_Zcmop'
+        if "Zba" in ispec["ISA"]:
+            self.isa += '_Zba'
+        if "Zbb" in ispec["ISA"]:
+            self.isa += '_Zbb'
+        if "Zbc" in ispec["ISA"]:
+            self.isa += '_Zbc'
+        if "Zbs" in ispec["ISA"]:
+            self.isa += '_Zbs'
+        if "Zbkb" in ispec["ISA"]:
+            self.isa += '_Zbkb'
+        if "Zbkc" in ispec["ISA"]:
+            self.isa += '_Zbkc'
+        if "Zknd" in ispec["ISA"]:
+            self.isa += '_Zknd'
+        if "Zkne" in ispec["ISA"]:
+            self.isa += '_Zkne'
+        if "Zbkx" in ispec["ISA"]:
+            self.isa += '_Zbkx'
+        if "Zknh" in ispec["ISA"]:
+            self.isa += '_Zknh'
+        if "Zksh" in ispec["ISA"]:
+            self.isa += '_Zksh'
+        if "Zksed" in ispec["ISA"]:
+            self.isa += '_Zksed'
+
+        self.compile_cmd = self.compile_cmd + ' -mabi=' + (
+            'lp64 ' if 64 in ispec['supported_xlen'] else
+            ('ilp32e ' if "E" in ispec["ISA"] else 'ilp32 '))
+        if 'pmp-grain' in ispec['PMP']:
+            self.granularity = pow(2, ispec['PMP']['pmp-grain'] + 2)
+        else:
+            self.granularity = 4
+
+    def runTests(self, testList):
+        if os.path.exists(self.work_dir + "/Makefile." + self.name[:-1]):
+            os.remove(self.work_dir + "/Makefile." + self.name[:-1])
+        make = utils.makeUtil(
+            makefilePath=os.path.join(self.work_dir, "Makefile." +
+                                      self.name[:-1]))
+
+        make.makeCommand = 'make -k -j' + self.num_jobs
+
+        for testname in testList:
+            testentry = testList[testname]
+            test = testentry['test_path']
+            test_dir = testentry['work_dir']
+            elf = os.path.join(test_dir, "my.elf")
+
+            sig_file = os.path.join(test_dir, self.name[:-1] + ".signature")
+
+            compile_macros = ' -D' + " -D".join(testentry['macros'])
+
+            cmd = self.compile_cmd.format(
+                testentry['isa'].lower().replace('zicsr', ' ', 2), self.xlen,
+                test, elf, compile_macros)
+
+            if self.target_run:
+                simcmd = self.dut_exe + ' {0} -b {1} --signature={2} --signature-granularity=8'.format(
+                    self.sd_image, elf, sig_file)
+            else:
+                simcmd = 'echo "NO RUN"'
+
+            execute = 'cd {}; {}; {};'.format(testentry['work_dir'], cmd,
+                                              simcmd)
+
+            make.add_target(execute)
+
+        make.execute_all(self.work_dir, timeout=3600)
+
+        if not self.target_run:
+            raise SystemExit(0)
